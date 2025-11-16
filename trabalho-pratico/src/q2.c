c1b99a68052027f7baefe7febf5a61bedcd3fa4f
charolzz <a111179@alunos.uminho.pt>
2025-11-16 18:31:51 +0000

    query 2 funcional

--- trabalho-pratico/src/q2.c
+++ trabalho-pratico/src/q2.c
@@ -10,19 +10,6 @@
 #include "errors.h"
 
 // =====================================================
-// LIBERTAÇÃO DE MEMÓRIA
-// =====================================================
-
-void libertaAeronave(void *data) {
-    Aeronave *a = data;
-    if (!a) return;
-    g_free(a->identifier);
-    g_free(a->manufacturer);
-    g_free(a->model);
-    g_free(a);
-}
-
-// =====================================================
 // FUNÇÕES AUXILIARES
 // =====================================================
 
