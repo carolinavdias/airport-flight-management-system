5542e9b8596b9fdeeb5beb0f8b37dceb5f7ae651
a111661 <a111661@alunos.uminho.pt>
2025-12-03 12:30:39 +0000

    estruturas

--- trabalho-pratico/include/entidades/passangers.h
+++ trabalho-pratico/include/entidades/passangers.h
@@ -1,4 +1,28 @@
 #ifndef PASSANGERS_H
 #define PASSANGERS_H
 
+typedef struct data_def {
+    int ano, mes, dia;
+} Data;
+
+typedef enum {
+    GENERO_F,
+    GENERO_M,
+    GENERO_O,
+    GENERO_ERROR
+} Genero;
+
+typedef struct passageiros {
+    int id_passageiro;
+    char *primeiro_nome;
+    char *ultimo_nome;
+    Data data_nascimento;
+    char *nacionalidade;
+    Genero genero_passageiro;
+    char *email_passageiro;
+    char *telefone_passageiro;
+    char *morada_passageiro;
+    char *fotografia_passageiro;
+} Passageiros;
+
 #endif
--- trabalho-pratico/include/entidades/reservations.h
+++ trabalho-pratico/include/entidades/reservations.h
@@ -1,4 +1,20 @@
 #ifndef RESERVATIONS_H
 #define RESERVATIONS_H
 
+typedef struct voos_reservados {
+    char **lista_voos_reservados;
+    int n_voos;
+} Voos_reservados;
+
+typedef struct reservas {
+    char *id_reserva;
+    Voos_reservados reserva_lista; //lista
+    int id_pessoa_reservou; //reserva em nome de
+    char *lugar_reservado;
+    double preco_reserva;
+    bool bagagem_extra;
+    bool prioridade; //priority boarding
+    char *qr_code;
+} Reservas;
+
 #endif
